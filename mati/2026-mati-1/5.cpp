605a84b42068f5161efbbe1d27f7add2b2a6721e
annekyl <annekyl@foxmail.com>
2026-05-25 13:43:08 +0800

    5-25-1

--- mati/2026-mati-1/5.cpp
+++ mati/2026-mati-1/5.cpp
@@ -5,10 +5,43 @@ using namespace std;
 
 const int mod = 1e9 + 7;
 
+int qmi(int a, int b, int p) {
+    int res = 1;
+
+    a %= p;
+    while (b) {
+        if (b & 1)
+            res = res * a % p;
+        a = a * a % p;
+        b >>= 1;
+    }
+    return res;
+}
+
 void solve() {
-    int n,d;
-    cin>>n>>d;
-    
+    int n, d;
+    cin >> n >> d;
+    int dp[n + 1];
+    int s[n + 1] = {0};
+
+    if (d == 1) {
+        int ans = qmi(4, n, mod);
+        cout << ans << endl;
+        return;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        dp[i] = 1;
+
+        if (i - d > 0)
+            dp[i] = (dp[i] + s[i - d]) % mod;
+        if (i - d + 1 > 0)
+            dp[i] = (dp[i] + s[i - d + 1]) % mod;
+
+        s[i] = (s[i - 1] + dp[i]) % mod;
+    }
+    int ans = (2 * s[n] + 1) % mod;
+    cout << ans << endl;
 }
 
 signed main() {
