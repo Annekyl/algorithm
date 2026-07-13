b5c47ed3bc00510f778546eca99868e6db55a13f
annekyl <annekyl@foxmail.com>
2026-07-13 22:08:31 +0800

    7-13-3

--- AcWing/算法提高课/宠物小精灵之收服.cpp
+++ AcWing/算法提高课/宠物小精灵之收服.cpp
@@ -4,7 +4,7 @@
 #define FOR(i, a, b) for (int i = (a); i <= (b); i++)
 #define REF(i, a, b) for (int i = (a); i >= (b); i--)
 #define debug(x) cout << #x << " = " << x << '\n'
-	
+
 using namespace std;
 
 typedef pair<int, int> pii;
@@ -16,19 +16,41 @@ constexpr int mod = 998244353;
 const int N = 1e5 + 10;
 
 void solve() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    int w1[k], w2[k];
+    for (int i = 0; i < k; i++)
+        cin >> w1[i] >> w2[i];
 
+    int dp[n + 1][m + 1] = {0};
+    for (int i = 0; i < k; i++) {
+        for (int j = n; j >= w1[i]; j--) {
+            for (int p = m - 1; p >= w2[i]; p--) {
+                dp[j][p] = max(dp[j][p], dp[j - w1[i]][p - w2[i]] + 1);
+            }
+        }
+    }
+    int mx = dp[n][m - 1];
+    // cout<<mx<<endl;
+    // return;
+    int remain;
+    for (remain = 0; remain < m; remain++) {
+        if (dp[n][remain] == mx)
+            break;
+    }
+    cout << mx << ' ' << m - remain << endl;
 }
 
 signed main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
-	int t = 1;
-	// cin >> t;
-	while (t--) {
-		solve();
-	}
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
 
-	return 0;
+    return 0;
 }
