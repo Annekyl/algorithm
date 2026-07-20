a84fa00316156d0cda261d3ea6abfe0a67e39cfc
Annekyl <annekyl@foxmail.com>
2026-07-20 09:03:01 +0800

    7-20-1

--- newCoder/Round 153/小红的网格图构造_1_0.cpp
+++ newCoder/Round 153/小红的网格图构造_1_0.cpp
@@ -18,19 +18,100 @@ const int N = 1e5 + 10;
 void solve() {
     int n, m, k;
     cin >> n >> m >> k;
-    int mx = ((n + 1) / 2) * ((m + 1) / 2) + (n / 2) * (m - ((m + 1) / 2));
+
+    if (n == 1) {
+        int mx = (m + 1) / 2;
+        if (k > mx) {
+            cout << "No" << endl;
+        } else {
+            cout << "Yes" << endl;
+            for (int j = 1; j <= m; j++) {
+                if (k && (j & 1)) {
+                    cout << 1;
+                    k--;
+                } else
+                    cout << 0;
+            }
+        }
+        return;
+    }
+
+    if (m == 1) {
+        int mx = (n + 1) / 2;
+        if (k > mx) {
+            cout << "No" << endl;
+        } else {
+            cout << "Yes" << endl;
+            for (int i = 1; i <= n; i++) {
+                if (k && (i & 1)) {
+                    cout << 1 << endl;
+                    k--;
+                } else
+                    cout << 0 << endl;
+            }
+        }
+        return;
+    }
+
+    // 2*2及以上，至少要有1个连通块
+    if (k == 0) {
+        cout << "No" << endl;
+        return;
+    }
+
+    int mx = ((n + 1) / 2) * ((m + 1) / 2) + (n / 2) * (m / 2);
     if (k > mx) {
         cout << "No" << endl;
     } else {
         cout << "Yes" << endl;
+        vii g(n + 1, vi(m + 1, 0));
+        for (int i = 1; i <= n; i += 2) {
+            for (int j = 1; j <= m; j += 2) {
+                g[i][j] = 1;
+            }
+        }
+        int base = ((n + 1) / 2) * ((m + 1) / 2);
+        if (k > base) {
+            // cout << "----" << endl;
+            k -= base;
+            // 添加连通块，在偶数行偶数列放1
+            for (int i = 2; i <= n; i += 2) {
+                for (int j = 2; j <= m; j += 2) {
+                    if (k) {
+                        g[i][j] = 1;
+                        k--;
+                    }
+                }
+            }
+        } else if (k < base) {
+            k = base - k;
+            // 减少连通块
+            // 先在偶数行奇数列放1
+            for (int i = 2; i <= n; i += 2) {
+                if (i == n)
+                    continue;
+                for (int j = 1; j <= m; j += 2) {
+                    if (k) {
+                        g[i][j] = 1;
+                        k--;
+                    }
+                }
+            }
+            // 再在奇数行偶数列放1, m为偶数时j!=m
+            for (int i = 1; i <= n; i += 2) {
+                for (int j = 2; j <= m; j += 2) {
+                    if (j == m)
+                        continue;
+                    if (k) {
+                        g[i][j] = 1;
+                        k--;
+                    }
+                }
+            }
+        }
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (k && (i + j) % 2 == 0) {
-                    cout << 1;
-                    k--;
-                } else {
-                    cout << 0;
-                }
+                cout << g[i][j];
             }
             cout << endl;
         }
