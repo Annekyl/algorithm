77b66d11a057ff57337f39d6fc45dfe17127627e
annekyl <annekyl@foxmail.com>
2026-08-13 19:05:42 +0800

    8-13-2

---
+++ AtCoder/abc470/abc470b.cpp
@@ -0,0 +1,56 @@
+#include "bits/stdc++.h"
+// #include <bits/extc++.h>
+#define int long long
+#define endl '\n'
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define REF(i, a, b) for (int i = (a); i >= (b); i--)
+#define debug(x) cout << #x << " = " << x << '\n'
+
+using namespace std;
+// using namespace __gnu_pbds;
+
+// typedef tree<int, null_type, less<int>,
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+// typedef tree<int, int, less<int>,
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_map;
+
+// typedef tree<pair<int,int>, null_type, less<pair<int,int>>,
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+
+constexpr int INF = 0x3f3f3f3f3f3f3f3f;
+constexpr int mod = 998244353;
+const int N = 1e5 + 10;
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n + 1, 0);
+    for (int i = 1; i <= n; i++) {
+        int x;
+        cin >> x;
+        a[x]++;
+    }
+    int mx = 0;
+    for (int i = 0; i <= n; i++)
+        mx = max(mx, a[i]);
+    cout << n - mx << endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
