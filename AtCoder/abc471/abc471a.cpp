1981c9247248caeef20fd76e5bd4d4260c7a0350
annekyl <annekyl@foxmail.com>
2026-08-18 21:51:08 +0800

    8-18-3

---
+++ AtCoder/abc471/abc471a.cpp
@@ -0,0 +1,52 @@
+#include "bits/stdc++.h"
+// #include <bits/extc++.h>
+#define int long long
+#define endl '\n'
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define REF(i, a, b) for (int i = (a); i >= (b); i--)
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define debug(x) cout << #x << " = " << x << '\n'
+
+using namespace std;
+// using namespace __gnu_pbds;
+
+// typedef tree<int, null_type, less<int>,
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+// typedef tree<int, int, less<int>,
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_map;
+
+// typedef tree<pair<int,int>, null_type, less<pair<int,int>>,
+//             rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vii;
+
+constexpr int INF = 0x3f3f3f3f3f3f3f3f;
+constexpr int mod = 998244353;
+const int N = 1e5 + 10;
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    if (a + b == 9 || a - b == 9 || a * b == 9 || a / b == 9 && a % b == 0) {
+        cout << "Nine" << endl;
+    } else {
+        cout << "Nein" << endl;
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
